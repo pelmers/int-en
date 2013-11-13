77f95a854ebf8396a66900332a222f93a049cf8e
Peter Elmers <peter.elmers@rice.edu>
2013-11-13 17:30:49 -0600

    jinja requirement dropped

--- views.py
+++ views.py
@@ -1,15 +1,15 @@
-import webapp2, jinja2, os
+import webapp2, os
 from i2e import num2str
 
-JINJA_ENV = jinja2.Environment(
-        loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
-        extensions = ['jinja2.ext.autoescape'],
-        autoescape=True)
+#JINJA_ENV = jinja2.Environment(
+        #loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+        #extensions = ['jinja2.ext.autoescape'],
+        #autoescape=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENV.get_template('templates/index.html')
-        self.response.write(template.render({}))
+        with open('html/index.html') as index:
+            self.response.write(index.read())
 
     def post(self):
         self.response.write(num2str(self.request.get("num")))
