ba6b0385a90190dc3fdd448c245bbaaa0a8acae8
Peter Elmers <peter.elmers@yahoo.com>
2014-01-06 20:58:36 -0600

    refactor and simplify

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import views
+
 import webapp2
+from int2en import int2en
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        with open('html/index.html') as index:
+            self.response.write(index.read())
+
+    def post(self):
+        self.response.write(int2en(self.request.get("num")))
 
 app = webapp2.WSGIApplication([
-    ('/', views.MainHandler),
+    ('/', MainHandler),
 ], debug=False)
